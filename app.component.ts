5a5cf867b7603776fc41d055dfedd2b17c235e24
Kishore <110915173+kishoresee@users.noreply.github.com>
2025-06-12 11:53:47 +0530

    Add files via upload

--- app.component.ts
+++ app.component.ts
@@ -1,41 +1,16 @@
 import { Component } from '@angular/core';
+import { RectComponent } from './rect/rect.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
+  imports: [RectComponent, FormsModule],
 })
 export class AppComponent {
-  dummyTrafficData = [
-    {
-      id: 'd1',
-      value: 433,
-    },
-    {
-      id: 'd2',
-      value: 260,
-    },
-    {
-      id: 'd3',
-      value: 290,
-    },
-    {
-      id: 'd4',
-      value: 410,
-    },
-    {
-      id: 'd5',
-      value: 397,
-    },
-    {
-      id: 'd6',
-      value: 488,
-    },
-    {
-      id: 'd47',
-      value: 589,
-    },
-  ];
-  maxTraffic = Math.max(...this.dummyTrafficData.map((data) => data.value));
-  currentStatus = 'online';
+  rectSize = {
+    width: '100',
+    height: '100',
+  };
 }
